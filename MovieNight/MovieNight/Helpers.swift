a2dcd0751cc27771af088c2b5b44f0979a5e664f
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-01-25 09:18:13 -0500

    Working on imageGetter functionality

--- MovieNight/MovieNight/Helpers.swift
+++ MovieNight/MovieNight/Helpers.swift
@@ -29,3 +29,51 @@ public extension Sequence where Iterator.Element: Hashable {
         return Array( Set(self) )
     }
 }
+
+class imageGetter {
+    
+    let urlString: String
+    let url: URL?
+    
+    init(urlString: String) {
+        self.urlString = urlString
+        
+        if let url = URL(string: urlString){
+            self.url = url
+        } else {
+            self.url = nil
+        }
+    }
+    
+    func getimageDataFromURL(completion: @escaping(_ data: Data?, _ response: URLResponse?, _ error: Error?) -> Void) {
+        
+        guard let url = url else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            completion(data, response, error)
+        }.resume()
+        
+    }
+    
+    func getImage(completion: @escaping(_ image: UIImage?) -> Void) {
+        
+        getimageDataFromURL { (data, response, error) in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if let image = UIImage(data: data) {
+                    completion(image)
+                } else {
+                    completion(nil)
+                }
+            }
+            
+        }
+        
+    }
+    
+}
